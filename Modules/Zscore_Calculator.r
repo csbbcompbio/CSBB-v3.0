c04aa04b151bf89eef9e1e8bd590fc2b847286e5
CSBB <skygenomics@gmail.com>
2018-05-10 13:36:13 -0400

    Update Zscore_Calculator.r

--- Modules/Zscore_Calculator.r
+++ Modules/Zscore_Calculator.r
@@ -6,6 +6,7 @@ file <- filenameR
 data <- as.matrix(read.table(file,sep="\t",header=T,row.names=1, check.names=F))
 print ("This is how input file looks")
 head(data)
-zscore_data <- as.matrix((data-mean(data))/sd(data))
+data1 <- scale(data, center = TRUE, scale = TRUE)
+zscore_data <- as.matrix((data1-mean(data1))/sd(data1))
 col <- cbind(row.names(data),zscore_data)
 write.table(col,"temporaryfile.txt",sep="\t",row.names=F,quote=F)
